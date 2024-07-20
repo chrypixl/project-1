8ce1d47957480044d1fb5c534cddb9bcfc9d0281
Jarvis <jarvwashington@gmail.com>
2024-07-20 12:05:49 -0500

    forgot to uncomment window.location.href

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ const getUserRepos = async function (title, category, author, genre) {
     await getBook(2);
     await getBook(3);
     await getBook(4);
-    //window.location.href = "./results.html";
+    window.location.href = "./results.html";
     // createBookCard(0);  This line should no longer be necessary. The function createBookCard is on results.js
 };
 
